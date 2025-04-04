c86eaed726aa35dd35c8318ef42f840f71c13a97
leonardo-toffalini <leonardotoffalini@gmail.com>
2025-04-04 15:12:50 +0200

    wip

--- lua/options.lua
+++ lua/options.lua
@@ -5,3 +5,12 @@ require "nvchad.options"
 local o = vim.o
 -- o.cursorlineopt = "both" -- to enable cursorline!
 o.relativenumber = true
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
